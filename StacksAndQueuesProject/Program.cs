a839d9bfa00c636829ddfe069cd40dd518be34df
BlaKe00786 <sameershaikh403@gmail.com>
2020-10-26 14:51:57 +0530

    [Sameer]Add: function to add nodes in queue.

--- StacksAndQueuesProject/Program.cs
+++ StacksAndQueuesProject/Program.cs
@@ -6,15 +6,11 @@ namespace StacksAndQueuesProject
     {
         static void Main(string[] args)
         {
-            Stack stack = new Stack();
-            int data;
-            //Console.WriteLine("Enter value: ");
-            //data = Convert.ToInt32(Console.ReadLine());
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            stack.Display();
-            stack.IsEmpty();
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
         }
     }
 }
